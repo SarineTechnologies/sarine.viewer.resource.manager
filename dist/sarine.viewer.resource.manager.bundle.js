ffea1d601897e2ea5b750c856cbcb10b2eaa4b0a
aharon.amram <aharon.amram@sarine.com>
2016-04-13 10:18:04 +0300

    adding dist folder

--- dist/sarine.viewer.resource.manager.bundle.js
+++ dist/sarine.viewer.resource.manager.bundle.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.resource.manager - v0.8.0 -  Wednesday, April 13th, 2016, 10:07:25 AM 
+sarine.viewer.resource.manager - v0.8.0 -  Wednesday, April 13th, 2016, 10:16:00 AM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
